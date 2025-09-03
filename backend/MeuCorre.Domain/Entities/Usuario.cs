7e8ed6e10ec779dc0eea10d6d7898aad4c5ffaa4
ricardomirandaa <riccardoomirandaa@gmail.com>
2025-09-02 21:42:25 -0300

    feat(backend)#3: configura entidade de usuario

---
+++ backend/MeuCorre.Domain/Entities/Usuario.cs
@@ -0,0 +1,66 @@
+﻿namespace MeuCorre.Domain.Entities
+{
+    public class Usuario : Entidade
+    {
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public string Senha { get; private set; }
+        public DateTime DataNascimento { get; private set; }
+        public bool Ativo { get; private set; }
+
+        //Construtor para criar um novo usuário.
+        //Construtor é a primeira coisa que é executada quando uma classe é instanciada.
+        public Usuario(string nome, string email, DateTime dataNascimento, bool ativo)
+        {
+            if (!TemIdadeMinima())
+            {
+                throw new Exception("Usuário deve ter no mínimo 13 anos.");
+            }
+
+            Nome = nome;
+            Email = email;
+            Senha = ValidarSenha(Senha);
+            DataNascimento = dataNascimento;
+            Ativo = ativo;
+        }
+
+        //Regra negocio: Permite apenas usários maiores de 13 anos.
+        private int CalcularIdade()
+        {
+            var hoje = DateTime.Today;
+            var idade = hoje.Year - DataNascimento.Year;
+
+            if (DataNascimento.Date > hoje.AddYears(-idade))
+                idade--;
+
+            return idade;
+        }
+
+        private bool TemIdadeMinima()
+        {
+            var resultado = CalcularIdade() >= 13;
+            return resultado;
+        }
+
+        public string ValidarSenha(string senha)
+        {
+            if(Senha.Length < 6)
+            {
+
+            }        
+            return senha;
+        }
+
+        public void AtivarUsuario()
+        {
+            Ativo = true;
+            AtualizarDataMoficacao();
+        }
+
+        public void InativarUsuario()
+        {
+            Ativo = false;
+            AtualizarDataMoficacao();
+        }
+    }
+}
---
+++ backend/MeuCorre.Infra/Data/Configurations/UsuarioConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using MeuCorre.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuCorre.Infra.Data.Configurations
+{
+    class UsuarioConfiguration : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            //Define o nome da tabela no banco de dados.
+            builder.ToTable("Usuarios");
+
+            //Define a chave primária.
+            builder.HasKey(usuario => usuario.Id);
+
+            //Define as propriedades da entidade e suas configurações.
+            builder.Property(usuario => usuario.Nome).IsRequired().HasMaxLength(100);
+
+            builder.Property(usuario => usuario.Email).IsRequired().HasMaxLength(255);
+
+            builder.Property(usuario => usuario.Senha).IsRequired();
+
+            builder.Property(usuario => usuario.DataNascimento).IsRequired();
+
+            builder.Property(usuario => usuario.Ativo).IsRequired();
+
+            builder.Property(usuario => usuario.DataCriacao).IsRequired();
+
+            builder.Property(usuario => usuario.DataAtualizacao).IsRequired(false);
+
+            builder.Property(usuario => usuario.Email);
+        }
+    }
+}

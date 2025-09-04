a4bda95e8c00471c9c5b341a17440b821fc4388e
ricardomirandaa <riccardoomirandaa@gmail.com>
2025-09-03 21:53:19 -0300

    chore(backend): adiciona migração

--- backend/MeuCorre.Infra/Data/Context/MeuDbContext.cs
+++ backend/MeuCorre.Infra/Data/Context/MeuDbContext.cs
@@ -17,5 +17,12 @@ namespace MeuCorre.Infra.Data.Context
         //Define a ligação entre a classe c# com a tabela do DB.
         public DbSet<Usuario> Usuarios { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //Aplica todas as configurações de mapeamento de entidades
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuDbContext).Assembly);
+        }
     }
 }

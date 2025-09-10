8ed0d6f27c4c0b74c772717b97bff17e918e2c51
ricardomirandaa <riccardoomirandaa@gmail.com>
2025-09-09 21:29:53 -0300

    feat: adiciona controller e command de usuário

---
+++ backend/MeuCorre.Application/UseCases/Usuarios/Commands/CriarUsuarioCommand.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuCorre.Application.UseCases.Usuarios.Commands
+{
+    /// <summary>
+    /// Comando para criar um novo usuário.
+    /// Aqui você pode adicionar propriedades necessárias para a criação do usuário, como Nome, Email, Senha, etc.
+    /// </summary>
+    public class CriarUsuarioCommand : IRequest<string>
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public DateTime DataNascimento { get; set; }
+    }
+}
---
+++ backend/MeuCorre/Controllers/UsuarioController.cs
@@ -0,0 +1,20 @@
+﻿using MeuCorre.Application.UseCases.Usuarios.Commands;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeuCorre.Controllers
+{ 
+    [ApiController]
+    [Route("[Controller]")]
+    public class UsuarioController
+    {
+        ///<summary>
+        ///Cria um novo usuário.
+        ///<param name="command"></param>
+        ///</summary>
+        [HttpPost]
+        public async Task<IActionResult> CriarUsuario([FromBody] CriarUsuarioCommand command)
+        {
+
+        }
+    }
+}

e59ec7b8e60ef365251869b74c0385102a299599
ricardomirandaa <riccardoomirandaa@gmail.com>
2025-09-17 21:50:41 -0300

    feat(backend):implementa use case para criar, atualizar e remover categorias.

--- backend/MeuCorre.Application/UseCases/Categorias/Commands/DeletarCategoriaCommand.cs
+++ backend/MeuCorre.Application/UseCases/Categorias/Commands/DeletarCategoriaCommand.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using MediatR;
+using MeuCorre.Domain.Enums;
+using MeuCorre.Domain.Interfaces.Repositories;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MeuCorre.Application.UseCases.Categorias.Commands
 {
-    internal class DeletarCategoriaCommand
+    internal class DeletarCategoriaCommand : IRequest<(string, bool)>
     {
+        [Required(ErrorMessage = "Id da categoria é obrigatório.")]
+        public required Guid CategoriaId { get; set; }
+
+        [Required(ErrorMessage = "Nome da categoria é obrigatório.")]
+        public required Guid UsuarioId { get; set; }
+    }
+
+    internal class DeletarCategoriaCommandHandler : IRequestHandler<DeletarCategoriaCommand, (string, bool)>
+    {
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public DeletarCategoriaCommandHandler(ICategoriaRepository categoriaRepository)
+        {
+            _categoriaRepository = categoriaRepository;
+        }
+
+        public async Task<(string, bool)> Handle(DeletarCategoriaCommand request, CancellationToken cancellationToken)
+        {
+            var categoria = await _categoriaRepository.ObterPorIdAsync(request.CategoriaId);
+            if (categoria == null)
+                return ("Categoria não encontrada.", false);
+
+            if (categoria.UsuarioId != request.UsuarioId)
+                return ("Categoria não pertence ao usuário.", false);
+
+            await _categoriaRepository.RemoverAsync(categoria);
+            return ("Categoria deletada com sucesso.", true);
+        }
     }
 }
